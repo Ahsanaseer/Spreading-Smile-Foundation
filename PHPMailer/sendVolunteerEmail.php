133b4f261f8efdc5dce477924959148b49c1c26b
Ahsanaseer <ahsanaseer058@gmail.com>
2025-10-16 05:45:36 +0500

    Small Changes

--- PHPMailer/sendVolunteerEmail.php
+++ PHPMailer/sendVolunteerEmail.php
@@ -51,6 +51,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             <meta charset='UTF-8'>
             <meta name='viewport' content='width=device-width, initial-scale=1.0'>
             <title>Thank You - Spreading Smile Foundation</title>
+            <link href='https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap' rel='stylesheet'>
             <style>
                 body {
                     font-family: 'Poppins', Arial, sans-serif;
